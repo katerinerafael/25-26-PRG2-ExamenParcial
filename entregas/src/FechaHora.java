7fb88943b8c1a2f19f06b329c19c3ce4b0f907cd
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-04-06 08:50:35 +0000

    refactor: reorganizar atributos y constructores en la clase FechaHora

--- entregas/src/FechaHora.java
+++ entregas/src/FechaHora.java
@@ -1,4 +1,14 @@
 class FechaHora {
-    public FechaHora(int dia, int mes, int año, int hora){}
-    public FechaHora(int hora){}
+    private int dia;
+    private int mes;
+    private int año;
+    private int hora;
+
+    public FechaHora(int dia, int mes, int año, int hora){
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
+        this.hora = hora;}
+    public FechaHora(int hora){
+        this.hora = hora;}
 }
