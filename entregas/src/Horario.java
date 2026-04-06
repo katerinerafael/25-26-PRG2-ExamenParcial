af38cff2773b2e0c2f1a194d8e717488467d2fbd
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-04-06 08:49:28 +0000

    refactor: mejorar la implementación de la clase Horario

--- entregas/src/Horario.java
+++ entregas/src/Horario.java
@@ -1,3 +1,11 @@
 class Horario{
-    public Horario(String diaInicio, String diaFinal, int horaInicio, int horaFinal){}
+    private String diaInicio, diaFinal;
+    private int horaInicio, horaFinal;
+
+    public Horario(String diaInicio, String diaFinal, int horaInicio, int horaFinal){
+        this.diaInicio = diaInicio;
+        this.diaFinal = diaFinal;
+        this.horaInicio = horaInicio;
+        this.horaFinal = horaFinal;
+    }
 }
